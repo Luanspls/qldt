c079decffe292ab239b417e0a6740bbd6cb01e1a
Luanspls <luannc.spls@gmail.com>
2025-10-25 18:06:17 +0700

    Fix: use QldtWeb.wsgi instead of app

--- QldtWeb/urls.py
+++ QldtWeb/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import health_check, home
+from .views import health_check
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
