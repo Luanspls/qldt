9926e24ec3b20ac1749d0b8f5ee80d0359c6784e
Luanspls <luannc.spls@gmail.com>
2025-10-26 10:19:53 +0700

    Fix settings file

--- QldtWeb/settings.py
+++ QldtWeb/settings.py
@@ -105,26 +105,26 @@ TEMPLATES = [
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": os.environ.get('DB_NAME', 'postgres'),
-    #     "USER": os.environ.get('DB_USER', 'postgres'),
-    #     "PASSWORD": os.environ.get('DB_PASSWORD'),
-    #     "HOST": os.environ.get('DB_HOST'),
-    #     "PORT": '6543', # os.environ.get('DB_PORT', '6543'),
-    #     'SCHEMA': 'public',
-    #     'OPTIONS': {
-    #         'connect_timeout': 30,
-    #         'keepalives': 1,
-    #         'keepalives_idle': 30,
-    #         'keepalives_interval': 10,
-    #         'keepalives_count': 5,
-    #     },
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get('DB_NAME', 'postgres'),
+        "USER": os.environ.get('DB_USER', 'postgres'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": os.environ.get('DB_PORT', '6543'),
+        'SCHEMA': 'public',
+        'OPTIONS': {
+            'connect_timeout': 30,
+            'keepalives': 1,
+            'keepalives_idle': 30,
+            'keepalives_interval': 10,
+            'keepalives_count': 5,
+        },
+    }
 }
 
 AUTHENTICATION_BACKENDS = [
