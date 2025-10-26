7f9a0a760dff67cc88642905726237c1f2571caa
Luanspls <luannc.spls@gmail.com>
2025-10-26 14:54:01 +0700

    add debug_setup to check

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('users/', views.users_list, name='users-list'),
+    path('health/', views.health_check, name='health-check'),
     path('debug-setup/', views.debug_setup, name='debug-setup'),
 ]
