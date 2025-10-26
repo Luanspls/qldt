90e706117b892c7b4b447a6bbb2afd532cb4a770
Luanspls <luannc.spls@gmail.com>
2025-10-26 11:05:06 +0700

    Fix settings file

--- QldtWeb/settings.py
+++ QldtWeb/settings.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
