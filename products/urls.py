1f6df8801a01487d426063a2f7894ba40b0c41bb
Luanspls <134633445+Luanspls@users.noreply.github.com>
2025-10-30 19:21:28 +0700

    Add train_program route to URLs

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('users/', views.users_list, name='users-list'),
     path('health/', views.health_check, name='health-check'),
     path('debug-setup/', views.debug_setup, name='debug-setup'),
+    path('train_program', views.train_program, name='train_program'),
 ]
