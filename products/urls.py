66615c787eefa6ffa8363944bb4df72773ff09e9
Luanspls <134633445+Luanspls@users.noreply.github.com>
2025-10-30 21:48:33 +0700

    Update URL paths for train program and import views

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('users/', views.users_list, name='users-list'),
     path('health/', views.health_check, name='health-check'),
     path('debug-setup/', views.debug_setup, name='debug-setup'),
-    path('train_program', views.train_program, name='train_program'),
+    path('train_program_manager', views.TrainProgramManagerView.as_view(), name='train_program_manager'),
+    path('import-excel/', views.ImportExcelView.as_view(), name='import_excel'),
+    path('thong-ke/', views.ThongKeView.as_view(), name='thong_ke'),
+    path('mon-hoc/<int:id>/', views.TrainProgramManagerView.as_view(), name='update_mon_hoc'),
 ]
