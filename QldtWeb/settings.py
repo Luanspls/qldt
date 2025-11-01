26ce6a85caf4e22f5ca6e2494cc9a9c5ba08e98b
Luanspls <luannc.spls@gmail.com>
2025-11-01 16:36:19 +0700

    Fix all

--- QldtWeb/settings.py
+++ QldtWeb/settings.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 from pathlib import Path
 from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
