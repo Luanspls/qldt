51f426305bcfd6ad94f07806798a5f2f65fba46c
Luanspls <luannc.spls@gmail.com>
2025-11-02 15:21:20 +0700

    Fix changes urls

--- QldtWeb/urls.py
+++ QldtWeb/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from django.http import HttpResponse
 from .views import health_check
 
-def home(request):
-    return HttpResponse("QLDT App is working!")
+# def home(request):
+#     return HttpResponse("QLDT App is working!")
 
 urlpatterns = [
-    path('', home, name='home'),
+    # path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('health/', health_check, name='health-check'),
     path('', include('products.urls')),
--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from QldtWeb.views import health_check
 
 
 urlpatterns = [
-    # path('', views.home_page, name='home'),
+    path('', views.home_page, name='home'),
     path('users/', views.users_list, name='users-list'),
     path('health/', health_check, name='health-check'),
     path('train_program_manager/', views.TrainProgramManagerView.as_view(), name='train_program_manager'),
