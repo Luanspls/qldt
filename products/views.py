1dc5553009cb7d97104a3e18b7b4dfacdf8a260f
Luanspls <134633445+Luanspls@users.noreply.github.com>
2025-11-08 20:15:18 +0700

    Update views.py

--- products/views.py
+++ products/views.py
@@ -896,8 +896,8 @@ def api_create_subject(request):
             
             # Tạo CurriculumSubject
             curriculum_subject = CurriculumSubject.objects.create(
-                curriculum=curriculum,
-                subject=subject,
+                curriculum=curiculum.id,
+                subject=subject.id,
                 credits=float(data['credits']),
                 total_hours=int(data.get('total_hours', 0) or 0),
                 theory_hours=int(data.get('theory_hours', 0) or 0),
